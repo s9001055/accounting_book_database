ef82d30da6ff110d169a5d61c9904bd61fad5af1
Arthur Chang <s9001055@gmail.com>
2022-07-24 22:58:11 +0800

    add group.h

---
+++ src/group.h
@@ -0,0 +1,52 @@
+#pragma once
+
+class group {
+    private:
+        int gid;
+        unsigned int number_of_people;
+        vector<item> item_list;
+        vector<people> people_list;
+        vector<pay_list> pay_list;
+    public:
+        group(int arg_gid);
+        ~group();
+        void add_people_to_group(people arg_people);
+        int create_item(string arg_name, vector<int> arg_people_pay, vector<people> arg_people);
+        void put_item_data(int arg_iid, vector<int> arg_people_pay, vector<people> arg_people);
+        item* get_item_data(int arg_iid);
+        vector<pay_list> pay_list_return();
+};
+
+group::group(int id) {
+    gid = id;
+    number_of_people = 0;
+}
+
+void group::add_people_to_group(people arg_people) {
+    number_of_people += 1;
+    people_list.push_back(arg_people);
+}
+
+int group::create_item(string arg_name, vector<int> arg_people_pay, vector<people> arg_people) {
+    int new_iid = item_list.size();
+    
+    item new_item(new_iid, arg_name, arg_people_pay, arg_people);
+    item_list.push_back(new_item);
+    return new_iid;
+}
+
+void group::put_item_data(int arg_iid, vector<int> arg_people_pay, vector<people> arg_people) {
+    item_list[arg_iid].modify_data(arg_people_pay, arg_people);
+}
+
+item* group::get_item_data(int arg_iid) {
+    item* ptr_item;
+    
+    ptr_item = &item_list[arg_iid];
+    return ptr_item;
+}
+
+vector<pay_list> group::pay_list_return() {
+    // un-finish
+
+}
